74c67a7aa6f6f9eb59be2880ed270eab3382ecdc
kadirbabaoglu <kadirbabaoglu@outlook.com>
2024-02-04 22:40:36 +0300

    güncelleme silme detay ve listeleme api si eklendi

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -14,4 +14,50 @@ const createProduct = async(req , res) => {
         } 
 }
 
-module.exports = {createProduct}
+const updateProduct = async(req,res) => {
+    const {id} = req.params
+    try {
+        await productModel.findByIdAndUpdate(id, req.body)
+        res.status(200).json({
+            message : "Güncelleme başarılı"
+        })
+    } catch (error) {
+        throw new APIError('ürün eklenirken bir hata oluştu' , 404)
+    }
+}
+
+
+const deleteProduct = async(req , res ) => {
+    const {id} = req.params
+    try {
+    await productModel.findByIdAndDelete(id)
+    res.status(200).json({
+        message : "Silme işlemi başarılı"
+    })
+    } catch (error) {
+        throw new APIError('ürün silinirken bir hata oluştu' , 404)
+    }
+}
+
+const getallProduct = async(req, res) => {
+    try {
+        const allproduct = await productModel.find()
+        res.status(200).json(allproduct)
+    } catch (error) {
+        throw new APIError('hiç ürün yok' , 404)
+    }
+    
+
+}
+
+const detailProduct = async(req, res) => {
+    const {id} = req.params
+    try {
+        const detail = await productModel.findById(id)
+        res.status(200).json(detail)
+    } catch (error) {
+        throw new APIError('ürün bulunamadı' , 404)
+    }
+}
+
+module.exports = {createProduct , updateProduct , deleteProduct , getallProduct , detailProduct}
--- src/router/productRoute.js
+++ src/router/productRoute.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
-const {createProduct} = require("../controller/productController")
-
-
+const {createProduct ,updateProduct , deleteProduct , getallProduct , detailProduct} = require("../controller/productController")
 
 router.post("/add-product" , createProduct)
-
+router.get("/detail-product/:id" , detailProduct)
+router.put("/update-product/:id" , updateProduct)
+router.delete("/delete-product/:id" , deleteProduct)
+router.get("/getall-product/" , getallProduct)
 
 module.exports = router
